96b60a68e7a8476bfdde5472e2eefc64c1d90780
ademrecber <ademrecber88@gmail.com>
2025-10-19 00:12:14 +0300

    Fix sitemap URLs

--- adretools/core/sitemaps.py
+++ adretools/core/sitemaps.py
@@ -6,60 +6,52 @@ class StaticViewSitemap(Sitemap):
     changefreq = 'weekly'
 
     def items(self):
-        # Tüm SEO URL'leri
+        # Sadece çalışan URL'ler
         return [
             # Ana sayfalar
             'core:home',
             
             # QR Tools
-            'qr_tools:qr_generator_tr', 'qr_tools:qr_generator_en',
-            'qr_tools:barcode_generator_tr', 'qr_tools:barcode_generator_en',
+            'qr_tools:home',
             
             # Image Tools  
-            'image_tools:resize_image_tr', 'image_tools:resize_image_en',
-            'image_tools:crop_image_tr', 'image_tools:crop_image_en',
-            'image_tools:compress_image_tr', 'image_tools:compress_image_en',
+            'image_tools:home',
             
             # PDF Tools
-            'pdf_tools:split_pdf_seo',
+            'pdf_tools:home',
             
             # Password Tools
-            'password_tools:password_generator_tr', 'password_tools:password_generator_en',
-            'password_tools:strong_password_tr', 'password_tools:strong_password_en',
+            'password_tools:home',
             
             # Calculator Tools
-            'calculator_tools:bmi_calculator_tr', 'calculator_tools:bmi_calculator_seo',
-            'calculator_tools:age_calculator_tr', 'calculator_tools:age_calculator_seo',
-            'calculator_tools:world_clock_tr', 'calculator_tools:world_clock_seo',
+            'calculator_tools:home',
             
             # Random Tools
-            'random_tools:random_number_tr', 'random_tools:random_number_seo',
-            'random_tools:lucky_wheel_tr', 'random_tools:lucky_wheel_seo',
-            'random_tools:name_picker_tr', 'random_tools:name_picker_seo',
+            'random_tools:home',
             
             # Text Tools
-            'text_tools:word_counter_tr', 'text_tools:word_counter_en',
-            'text_tools:text_analyzer_tr', 'text_tools:text_analyzer_en',
+            'text_tools:home',
             
             # URL Tools
-            'url_tools:url_shortener_tr', 'url_tools:url_shortener_en',
-            'url_tools:link_shortener_tr', 'url_tools:link_shortener_en',
+            'url_tools:home',
             
             # Color Tools
-            'color_tools:color_picker_tr', 'color_tools:color_picker_en',
-            'color_tools:hex_color_tr', 'color_tools:hex_color_en',
+            'color_tools:home',
             
             # Converter Tools
-            'converter_tools:unit_converter_tr', 'converter_tools:unit_converter_en',
-            'converter_tools:temperature_converter_tr', 'converter_tools:temperature_converter_en',
+            'converter_tools:home',
             
             # SVG Tools
-            'svg_tools:svg_editor_tr', 'svg_tools:svg_editor_en',
-            'svg_tools:image_to_svg_tr', 'svg_tools:image_to_svg_en',
+            'svg_tools:home',
             
             # Horoscope Tools
-            'horoscope_tools:horoscope_calculator_tr', 'horoscope_tools:horoscope_calculator_en',
-            'horoscope_tools:birth_date_horoscope_tr', 'horoscope_tools:birth_date_horoscope_en',
+            'horoscope_tools:home',
+            
+            # Network Tools
+            'network_tools:home',
+            
+            # Dev Tools
+            'dev_tools:home',
         ]
 
     def location(self, item):
