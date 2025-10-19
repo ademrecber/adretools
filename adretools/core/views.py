f873eda2db8f9d1962b1766623868c3383b1a50c
ademrecber <ademrecber88@gmail.com>
2025-10-19 21:39:04 +0300

    Tüm proje güncellemeleri commit edildi

--- adretools/core/views.py
+++ adretools/core/views.py
@@ -20,9 +20,6 @@ def home(request):
     ]
     return render(request, 'core/home.html', {'tools': tools})
 
-def privacy_policy(request):
-    return render(request, 'privacy-policy.html')
-
 def terms_of_service(request):
     return render(request, 'terms-of-service.html')
 
