b82dfff754bfbd0a8efbe484dbf86762910e4cdc
ademrecber <ademrecber88@gmail.com>
2025-10-19 21:34:57 +0300

    Tüm proje güncellemeleri commit edildi

--- adretools/core/urls.py
+++ adretools/core/urls.py
@@ -5,4 +5,12 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
+    path('gizlilik-politikasi/', views.privacy_policy, name='privacy_policy_tr'),
+    path('terms-of-service/', views.terms_of_service, name='terms_of_service'),
+    path('kullanim-sartlari/', views.terms_of_service, name='terms_of_service_tr'),
+    path('cookie-policy/', views.cookie_policy, name='cookie_policy'),
+    path('cerez-politikasi/', views.cookie_policy, name='cookie_policy_tr'),
+    path('contact/', views.contact, name='contact'),
+    path('iletisim/', views.contact, name='contact_tr'),
 ]
