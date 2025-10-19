880146e5343a3df18279eddde947a81326b37200
ademrecber <ademrecber88@gmail.com>
2025-10-19 21:20:57 +0300

    Tüm proje güncellemeleri commit edildi

--- adretools/adretools/urls.py
+++ adretools/adretools/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     # SEO
     path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml', content_type='application/xml')),
     path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    path('ads.txt', TemplateView.as_view(template_name='ads.txt', content_type='text/plain')),
 ]
 
 urlpatterns += i18n_patterns(
