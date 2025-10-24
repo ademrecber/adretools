f37293ac370b9e76e8f37528178c7ba871db4007
ademrecber <ademrecber88@gmail.com>
2025-10-24 23:27:23 +0300

    URL yönlendirme middleware'i: HTTP→HTTPS, www→non-www redirects ve security ayarları

--- adretools/adretools/settings.py
+++ adretools/adretools/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'core.middleware.RedirectMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -116,3 +117,15 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Security Settings
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
--- adretools/core/middleware.py
+++ adretools/core/middleware.py
@@ -1,53 +1,25 @@
-from django.utils import translation
+from django.http import HttpResponsePermanentRedirect
 from django.conf import settings
 
-class AutoLanguageMiddleware:
+class RedirectMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # URL'den dil kodunu al
-        path_parts = request.path.strip('/').split('/')
-        url_language = None
+        # HTTP to HTTPS redirect
+        if not request.is_secure() and not settings.DEBUG:
+            return HttpResponsePermanentRedirect(
+                'https://' + request.get_host() + request.get_full_path()
+            )
         
-        if path_parts and path_parts[0] in dict(settings.LANGUAGES):
-            url_language = path_parts[0]
+        # www to non-www redirect
+        host = request.get_host()
+        if host.startswith('www.'):
+            new_host = host[4:]  # Remove 'www.'
+            protocol = 'https' if request.is_secure() else 'http'
+            return HttpResponsePermanentRedirect(
+                f'{protocol}://{new_host}{request.get_full_path()}'
+            )
         
-        # URL'de dil kodu varsa onu kullan
-        if url_language:
-            language = url_language
-            translation.activate(language)
-            request.LANGUAGE_CODE = language
-            request.session['django_language'] = language
-        # Session'da dil varsa onu kullan
-        elif 'django_language' in request.session:
-            language = request.session['django_language']
-            translation.activate(language)
-            request.LANGUAGE_CODE = language
-        else:
-            # Accept-Language header'ından dil algıla
-            accept_language = request.META.get('HTTP_ACCEPT_LANGUAGE', '')
-            
-            # Desteklenen diller
-            supported_languages = dict(settings.LANGUAGES)
-            
-            # En uygun dili bul
-            best_language = 'en'  # varsayılan
-            
-            if accept_language:
-                # Accept-Language: tr-TR,tr;q=0.9,en;q=0.8,en-US;q=0.7
-                languages = accept_language.split(',')
-                for lang in languages:
-                    lang_code = lang.split(';')[0].strip().split('-')[0].lower()
-                    if lang_code in supported_languages:
-                        best_language = lang_code
-                        break
-            
-            # Dili aktif et
-            translation.activate(best_language)
-            request.LANGUAGE_CODE = best_language
-            request.session['django_language'] = best_language
-
         response = self.get_response(request)
-        translation.deactivate()
         return response
